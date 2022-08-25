278a36b0ab9abf378afe4a9f8ce0b290b63ae48d
Samudra Perera <samudrapup@gmail.com>
2022-08-25 17:08:25 -0400

    Added styling, added conditional logic to dynamically remove existing cocktail cards from the DOM without a refresh

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
+//variables to store the input and search
 const drinkSearch = document.querySelector('.drinkSearch')
 const searchButton = document.querySelector('.searchButton')
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
 const fetchDrink = async () => {
     let drink = drinkSearch.value
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`
-    const res = await fetch(url)
-    const data = await res.json()
-    console.log(data)
-    displayData2(data)
+    try {
+        const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`
+        const res = await fetch(url)
+        const data = await res.json()
+        displayData2(data)
+    } catch {
+
+    }
 }
 
 searchButton.addEventListener('click', function() {
     fetchDrink()
 })
 
+//This function creates the cards using an HTML template that the API data will be stored in
 function createDrinkCard(i) {
     const cocktailEle = document.createElement('div')
     cocktailEle.classList.add('cocktail')
@@ -34,9 +40,16 @@ function createDrinkCard(i) {
 
 // This is the function that will display the data to the dynamically created HTML elements from CreateDrinksCard()
 function displayData2(data) {
+    //Checks if there is already cocktail cards completed (for a prev search) if there is deletes all the nodes 
+    if(document.querySelectorAll('.cocktail.slide').length) {0
+        //creates node list of all divs then deletes using forEach
+        let del = document.querySelectorAll('.cocktail.slide')
+        del.forEach(elem => elem.remove())
+    }
     //iterates through the drinks array and creates HTML elements
     for(let j = 0; j < data.drinks.length; j++) {
         // To ensure that the first element is displayed without hardcoding data-active = true into the cocktailInnerHtml
+        // Passes in j to createDrinkCard and which produces the template for the cocktail card for every element in the array
         createDrinkCard(j)
         if(j == 0) {
             document.querySelector('.slide').dataset.active = true
@@ -68,11 +81,11 @@ function displayData2(data) {
 }
 
 
-// Creating the carousel -- got this code from Youtube and repurposed my code to work with it
-const buttons = document.querySelectorAll("[data-carousel-button]")
-
+// Creating the carousel -- done using the help of youtube
 buttons.forEach(button => {
+    //adds event listener to each button
     button.addEventListener("click", () => {
+        //if button data = 'next' offset = 1 else offset = -1
        const offset = button.dataset.carouselButton === 'next' ? 1 : -1; 
        const slides = button
        .closest("[data-carousel]")
